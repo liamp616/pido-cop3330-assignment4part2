9130798e37703d141884f1b15eef4a660c6b542e
Liam <xlime1p@gmail.com>
2021-07-08 14:36:13 -0400

    initial solution (need to do export/import, tests, and make completed and distinct complete/incomplete)

--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -1,12 +1,49 @@
 package ucf.assignments;
 
+import java.time.LocalDate;
+
 public class Item {
-    //  task: String
-    //  dueDate: String
     //  desc: String
+    private String description;
+    //  dueDate: String
+    private LocalDate date;
     //  complete: boolean
+    private Boolean complete = false;
 
     //  create constructor
+    public Item(String description, LocalDate date) {
+        this.description = description;
+        this.date = date;
+    }
+
+    @Override
+    public String toString(){
+        //  return this.getDate() + "\t" + this.getDescription()+ "\t\t\t" + complete;
+        return String.format(this.getDate() + "\t" + "%-140s\t" + this.getComplete(), description);
+    }
 
     //  create getter and setter methods for variables
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
 }
--- src/main/java/ucf/assignments/ListManagerController.java
+++ src/main/java/ucf/assignments/ListManagerController.java
@@ -3,112 +3,182 @@ package ucf.assignments;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import java.awt.*;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ListManagerController implements Initializable {
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        datePicker.setValue(LocalDate.now());
     }
 
     //  make @FXML variables
-
-    //  make observable list
+    @FXML
+    Button addButton;
 
     @FXML
-    private void addList(ActionEvent actionEvent) {
-        //  get the title from titleTextField and a new eventList to the list with that title
+    Button removeButton;
+
+    @FXML
+    Button clearButton;
+
+    @FXML
+    Button updateButton;
+
+    @FXML
+    Button exportButton;
+
+    @FXML
+    Button importButton;
+
+    @FXML
+    Button markCompleteButton;
+
+    @FXML
+    Button markIncompleteButton;
+
+    @FXML
+    Button displayCompletedButton;
+
+    @FXML
+    Button displayIncompletedButton;
+
+    @FXML
+    Button displayAllButton;
+
+    @FXML
+    DatePicker datePicker;
+
+    @FXML
+    TextField descriptionTextField;
+
+    @FXML
+    ListView<Item> itemList;
+
+    //  create observable list
+    ObservableList<Item> list = FXCollections.observableArrayList();
+
+    //  refreshes the date picker and the description text box
+    private void refresh() {
+        datePicker.setValue(LocalDate.now());
+        descriptionTextField.setText(null);
+        itemList.refresh();
     }
 
     @FXML
-    private void removeList(ActionEvent actionEvent) {
+    private void addItem(Event e) {
+        //  make a new item and add it to the observable list
+        list.add(new Item(descriptionTextField.getText(), datePicker.getValue()));
+        //  add the observable list to the list view
+        itemList.setItems(list);
+        refresh();
+    }
+
+    @FXML
+    private void removeItem(Event e) {
         //  find the index of the selected item
+        int index = itemList.getSelectionModel().getSelectedIndex();
         //  remove the index of the list
+        itemList.getItems().remove(index);
+        refresh();
     }
 
     @FXML
-    private void handleRightClick(ActionEvent actionEvent) {
-        //  open a context menu when right clicking an element on the listview
+    private void clearAll(Event e) {
+        itemList.getItems().clear();
+        refresh();
+    }
+
+    public void listViewSelectedItem() {
+        itemList.setOnMouseClicked(event -> {
+            String selectedItem = itemList.getSelectionModel().getSelectedItem().getDescription();
+            int index = itemList.getSelectionModel().getSelectedIndex();
+            descriptionTextField.setText(selectedItem);
+            datePicker.setValue(itemList.getItems().get(index).getDate());
+        });
+        refresh();
     }
 
     @FXML
-    private void exportList(ActionEvent actionEvent) {
+    private void updateItem(Event e) {
+        int index = itemList.getSelectionModel().getSelectedIndex();
+        itemList.getItems().get(index).setDate(datePicker.getValue());
+        itemList.getItems().get(index).setDescription(descriptionTextField.getText());
+        refresh();
+    }
+
+    @FXML
+    private void exportList(Event e) {
         //  this can be used from the menu bar
         //  open up a save as window and can save the file
     }
 
     @FXML
-    private void exportAllLists(ActionEvent actionEvent) {
-        //  this can be used from the menu bar
-        //  open up a save as window and can save the file
-    }
-
-    @FXML
-    private void importList(ActionEvent actionEvent) {
+    private void importList(Event e) {
         //  this can be used from the menu bar
         //  open up an open window and can select the file
     }
 
     @FXML
-    private void importAllList(ActionEvent actionEvent) {
-        //  this can be used from the menu bar
-        //  open up an open window and can select the file
-    }
-
-    @FXML
-    private void renameList(ActionEvent actionEvent) {
-        //  this can be used from a right click
-        //  open up another window where user can input a new title
-    }
-
-    @FXML
-    private void addItem(ActionEvent actionEvent) {
-        //  this can be used from a right click to open a new window
-        //  open up another listview window where it will show all the items
-    }
-
-    @FXML
-    private void removeItem(ActionEvent actionEvent) {
+    private void markComplete(Event e) {
         //  find the index of the selected item
-        //  remove the index of the list
+        int index = itemList.getSelectionModel().getSelectedIndex();
+        //  change the value of the boolean to true
+        list.get(index).setComplete(true);
+        refresh();
+    }
+
+    @FXML void markIncomplete(Event e) {
+        //  find the index of the selected item
+        int index = itemList.getSelectionModel().getSelectedIndex();
+        //  change the value of the boolean to false
+        list.get(index).setComplete(false);
+        refresh();
     }
 
     @FXML
-    private void editDueDate(ActionEvent actionEvent) {
-        //  this can be used from a right click
-        //  open up another window where user can input a new due date
-    }
+    private void displayComplete(Event e) {
+        ObservableList<Item> complete = FXCollections.observableArrayList();
 
-    @FXML
-    private void editDesc(ActionEvent actionEvent) {
-        //  this can be used from a right click
-        //  open up another window where user can input a new due description
-    }
-
-    @FXML
-    private void markComplete(ActionEvent actionEvent) {
-        //  this can be used from a right click
-        //  mark the complete boolean to true for this item
-    }
-
-    @FXML
-    private void displayIncomplete(ActionEvent actionEvent) {
-        //  find all items that have the complete boolean to false
-        //  display those items
-    }
-
-    @FXML
-    private void displayComplete(ActionEvent actionEvent) {
         //  find all items that have the complete boolean to true
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).getComplete().equals(true)) {
+                complete.add(list.get(i));
+            }
+        }
         //  display those items
+        itemList.setItems(complete);
+        refresh();
+    }
+
+    @FXML
+    private void displayIncomplete(Event e) {
+        ObservableList<Item> incomplete = FXCollections.observableArrayList();
+
+        //  find all items that have the complete boolean to false
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).getComplete().equals(false)) {
+                incomplete.add(list.get(i));
+            }
+        }
+        //  display those items
+        itemList.setItems(incomplete);
+        refresh();
+    }
+
+    @FXML
+    private void displayAll(Event e) {
+        itemList.setItems(list);
+        refresh();
     }
 }
